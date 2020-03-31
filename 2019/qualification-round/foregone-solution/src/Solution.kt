678d278ffc7fd45c5e9b8af5a8b913134169ddf9
Shreck Ye <ShreckYe@gmail.com>
2020-04-01 00:32:18 +0800

    Improve code for Foregone Solution

--- 2019/qualification-round/foregone-solution/src/Solution.kt
+++ 2019/qualification-round/foregone-solution/src/Solution.kt
@@ -8,8 +8,8 @@ fun main() {
         }
 
         fun String.toReadableNumber(): String = trimStart('0').let { if (it.isEmpty()) "0" else it }
-        val A = digitPairs.map { it.first }.joinToString("").toReadableNumber()
-        val B = digitPairs.map { it.second }.joinToString("").toReadableNumber()
+        val A = digitPairs.asSequence().map { it.first }.joinToString("").toReadableNumber()
+        val B = digitPairs.asSequence().map { it.second }.joinToString("").toReadableNumber()
 
         println("Case #${i + 1}: $A $B")
     }
