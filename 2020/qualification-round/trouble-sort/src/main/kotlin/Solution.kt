20f117fe60d0e069811e455e620b39d8b415420d
Shreck Ye <ShreckYe@gmail.com>
2021-03-24 04:57:41 +0800

    Add a passed solution for Trouble Sort

--- 2020/qualification-round/trouble-sort/src/main/kotlin/Solution.kt
+++ 2020/qualification-round/trouble-sort/src/main/kotlin/Solution.kt
@@ -7,25 +7,17 @@ fun case(t: Int) {
     val N = readLine()!!.toInt()
     val Vs = readLine()!!.splitToSequence(' ').map { it.toInt() }.toList()
 
-    val array = Vs.toIntArray()
-    troubleSort(array)
-    val firstErrorIndex = array.asSequence().zipWithNext().indexOfFirst { it.first > it.second }
+    val (evenIndexNumsWI, oddIndexNumsWI) = Vs.withIndex().partition { it.index % 2 == 0 }
+    val evenIndexArray = evenIndexNumsWI.map { it.value }.toIntArray()
+    val oddIndexArray = oddIndexNumsWI.map { it.value }.toIntArray()
+
+    evenIndexArray.sort()
+    oddIndexArray.sort()
+
+    val troubleSortedArray = IntArray(N) { i -> if (i % 2 == 0) evenIndexArray[i / 2] else oddIndexArray[i / 2] }
+
+    val firstErrorIndex = troubleSortedArray.asSequence().zipWithNext().indexOfFirst { it.first > it.second }
 
     val y = if (firstErrorIndex == -1) "OK" else firstErrorIndex
     println("Case #${t + 1}: $y")
-}
-
-// 3 ≤ N
-fun troubleSort(array: IntArray) {
-    do {
-        var changed = false
-        for (i in 0 until array.size - 2)
-            if (array[i] > array[i + 2]) {
-                val t = array[i]
-                array[i] = array[i + 2]
-                array[i + 2] = t
-
-                changed = true
-            }
-    } while (changed)
 }
