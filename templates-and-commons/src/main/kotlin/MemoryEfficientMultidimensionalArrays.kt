998504177a0dca1ca4f679509fa519229ecac521
Shreck Ye <ShreckYe@gmail.com>
2021-04-03 22:16:50 +0800

    Simplify RandomAccess to use only one `create` function and add MemoryEfficientMultidimensionalArrays.kt

---
+++ templates-and-commons/src/main/kotlin/MemoryEfficientMultidimensionalArrays.kt
@@ -0,0 +1,56 @@
+import java.util.*
+
+class TwoD<T, C>(
+    val size1: Int, val size2: Int, val randomAccess: RandomAccess<T, C>, init: ((Int, Int) -> T)? = null
+) {
+    val backing = randomAccess.create(size1 * size2).also {
+        if (init !== null)
+            with(randomAccess) {
+                for (i in 0 until size1) {
+                    val size1TimesI = size1 * i
+                    for (j in 0 until size2)
+                        it[size1TimesI + j] = init(i, j)
+                }
+            }
+    }
+
+    fun backingIndex(i: Int, j: Int) = size1 * i + j
+    fun get(i: Int, j: Int): T = with(randomAccess) { backing[backingIndex(i, j)] }
+    operator fun set(i: Int, j: Int, value: T) = with(randomAccess) { backing[backingIndex(i, j)] = value }
+}
+
+fun twoDIntArray(size1: Int, size2: Int, init: ((Int, Int) -> Int)? = null): TwoD<Int, IntArray> =
+    TwoD(size1, size2, IntArrayRandomAccess, init)
+
+fun twoDLongArray(size1: Int, size2: Int, init: ((Int, Int) -> Long)? = null): TwoD<Long, LongArray> =
+    TwoD(size1, size2, LongArrayRandomAccess, init)
+
+fun twoDFloatArray(size1: Int, size2: Int, init: ((Int, Int) -> Float)? = null): TwoD<Float, FloatArray> =
+    TwoD(size1, size2, FloatArrayRandomAccess, init)
+
+fun twoDDoubleArray(size1: Int, size2: Int, init: ((Int, Int) -> Double)? = null): TwoD<Double, DoubleArray> =
+    TwoD(size1, size2, DoubleArrayRandomAccess, init)
+
+
+fun twoDBitSet(size1: Int, size2: Int, init: ((Int, Int) -> Boolean)? = null): TwoD<Boolean, BitSet> =
+    TwoD(size1, size2, BitSetRandomAccess, init)
+
+
+// These are not really much more efficient than native 2D arrays.
+
+fun <NotNullT : Any> twoDArrayOfNullables(
+    size1: Int, size2: Int, init: ((Int, Int) -> NotNullT?)? = null
+): TwoD<NotNullT?, Array<NotNullT?>> =
+    TwoD(size1, size2, NullableArrayRandomAccess(), init)
+
+fun <T : Any> twoDArrayOfNotNulls(
+    size1: Int, size2: Int, defaultValue: T
+): TwoD<T, Array<T>> =
+    TwoD(size1, size2, NotNullArrayRandomAccess(defaultValue), null)
+
+fun <T : Any> twoDArrayOfNotNulls(
+    size1: Int, size2: Int, init: ((Int, Int) -> T)
+): TwoD<T, Array<T>> {
+    @Suppress("UNCHECKED_CAST")
+    return twoDArrayOfNullables(size1, size2, init) as TwoD<T, Array<T>>
+}
--- templates-and-commons/src/main/kotlin/RandomAccess.kt
+++ templates-and-commons/src/main/kotlin/RandomAccess.kt
@@ -3,73 +3,78 @@
 import java.util.*
 
 interface RandomAccess<T, C> {
-    fun create(size: Int, init: (Int) -> T): C
+    /* `init` is not really necessary for coding competitions,
+    first because O(n) initialization doesn't add to the overall asymptotic complexity,
+    and second because array initialization is already O(n) on the JVM since JVM initializes its contents to the default value (0 or null). */
+    fun create(size: Int, init: ((Int) -> T)? = null): C
     operator fun C.get(i: Int): T
     operator fun C.set(i: Int, value: T)
 }
 
 
-// This is not really necessary for coding competitions,
-// first because O(n) initialization doesn't add to the overall asymptotic complexity,
-// and second because array initialization is already O(n) on the JVM since JVM initializes its contents to the default value (0 or null).
-interface RandomAccessWithDefaultValue<T, C> : RandomAccess<T, C> {
-    fun createWithDefaultValue(size: Int): C
-}
+object IntArrayRandomAccess : RandomAccess<Int, IntArray> {
+    override fun create(size: Int, init: ((Int) -> Int)?): IntArray =
+        if (init === null) IntArray(size) else IntArray(size, init)
 
-
-object IntArrayRandomAccess : RandomAccessWithDefaultValue<Int, IntArray> {
-    override fun create(size: Int, init: (Int) -> Int): IntArray = IntArray(size, init)
-    override fun createWithDefaultValue(size: Int): IntArray = IntArray(size)
     override operator fun IntArray.get(i: Int): Int = get(i)
     override operator fun IntArray.set(i: Int, value: Int) = set(i, value)
 }
 
-object LongArrayRandomAccess : RandomAccessWithDefaultValue<Long, LongArray> {
-    override fun create(size: Int, init: (Int) -> Long): LongArray = LongArray(size, init)
-    override fun createWithDefaultValue(size: Int): LongArray = LongArray(size)
+object LongArrayRandomAccess : RandomAccess<Long, LongArray> {
+    override fun create(size: Int, init: ((Int) -> Long)?): LongArray =
+        if (init === null) LongArray(size) else LongArray(size, init)
+
     override operator fun LongArray.get(i: Int): Long = get(i)
     override operator fun LongArray.set(i: Int, value: Long) = set(i, value)
 }
 
-object FloatArrayRandomAccess : RandomAccessWithDefaultValue<Float, FloatArray> {
-    override fun create(size: Int, init: (Int) -> Float): FloatArray = FloatArray(size, init)
-    override fun createWithDefaultValue(size: Int): FloatArray = FloatArray(size)
+object FloatArrayRandomAccess : RandomAccess<Float, FloatArray> {
+    override fun create(size: Int, init: ((Int) -> Float)?): FloatArray =
+        if (init === null) FloatArray(size) else FloatArray(size, init)
+
     override operator fun FloatArray.get(i: Int): Float = get(i)
     override operator fun FloatArray.set(i: Int, value: Float) = set(i, value)
 }
 
-object DoubleArrayRandomAccess : RandomAccessWithDefaultValue<Double, DoubleArray> {
-    override fun create(size: Int, init: (Int) -> Double): DoubleArray = DoubleArray(size, init)
-    override fun createWithDefaultValue(size: Int): DoubleArray = DoubleArray(size)
+object DoubleArrayRandomAccess : RandomAccess<Double, DoubleArray> {
+    override fun create(size: Int, init: ((Int) -> Double)?): DoubleArray =
+        if (init === null) DoubleArray(size) else DoubleArray(size, init)
+
     override operator fun DoubleArray.get(i: Int): Double = get(i)
     override operator fun DoubleArray.set(i: Int, value: Double) = set(i, value)
 }
 
 
-object BitSetRandomAccess : RandomAccessWithDefaultValue<Boolean, BitSet> {
-    override fun create(size: Int, init: (Int) -> Boolean): BitSet =
-        BitSet(size).also { for (i in 0 until size) it[i] = init(i) }
+object BitSetRandomAccess : RandomAccess<Boolean, BitSet> {
+    override fun create(size: Int, init: ((Int) -> Boolean)?): BitSet =
+        BitSet(size).also { if (init !== null) for (i in 0 until size) it[i] = init(i) }
 
-    override fun createWithDefaultValue(size: Int): BitSet = BitSet(size)
     override fun BitSet.get(i: Int): Boolean = get(i)
     override fun BitSet.set(i: Int, value: Boolean) = set(i, value)
 }
 
 
-open class ArrayRandomAccess<T> : RandomAccess<T, Array<T>> {
-    override fun create(size: Int, init: (Int) -> T): Array<T> {
+interface ArrayRandomAccess<T> : RandomAccess<T, Array<T>> {
+    fun createWithDefault(size: Int): Array<T>
+    override fun create(size: Int, init: ((Int) -> T)?): Array<T> {
         @Suppress("UNCHECKED_CAST")
-        return Array<Any?>(size, init) as Array<T>
+        return if (init === null) createWithDefault(size) else Array<Any?>(size, init) as Array<T>
     }
 
     override operator fun Array<T>.get(i: Int): T = get(i)
     override operator fun Array<T>.set(i: Int, value: T) = set(i, value)
 }
 
-class ArrayRandomAccessWithNullDefaultValue<NotNullT : Any> :
-    ArrayRandomAccess<NotNullT?>(), RandomAccessWithDefaultValue<NotNullT?, Array<NotNullT?>> {
-    override fun createWithDefaultValue(size: Int): Array<NotNullT?> {
+class NullableArrayRandomAccess<NotNullT : Any> : ArrayRandomAccess<NotNullT?> {
+    override fun createWithDefault(size: Int): Array<NotNullT?> {
         @Suppress("UNCHECKED_CAST")
-        return arrayOfNulls<Any?>(size) as Array<NotNullT?>
+        return arrayOfNulls<Any>(size) as Array<NotNullT?>
+    }
+}
+
+class NotNullArrayRandomAccess<T : Any>(val defaultValue: T) : ArrayRandomAccess<T> {
+    override fun createWithDefault(size: Int): Array<T> {
+        @Suppress("UNCHECKED_CAST")
+        return Array<Any>(size) { defaultValue } as Array<T>
     }
 }
